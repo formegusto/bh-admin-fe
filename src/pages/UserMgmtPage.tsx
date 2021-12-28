2a7626e4c470ba91884267c3ed9dbfea243ac9e5
formegusto <formegusto@gmail.com>
2021-12-28 10:50:01 +0900

    ui make library installed

---
+++ src/pages/UserMgmtPage.tsx
@@ -0,0 +1,5 @@
+function UserMgmtPage() {
+  return <>유저 관리 페이지</>;
+}
+
+export default UserMgmtPage;
---
+++ src/templates/BaseTemplate.tsx
@@ -0,0 +1,5 @@
+function BaseTemplate({ children }: React.PropsWithChildren<any>) {
+  return <>{children}</>;
+}
+
+export default BaseTemplate;
